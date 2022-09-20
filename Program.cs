37e66374140c75dedf685c1df4e3140fd96c346d
Deykun <maslovsasapvp@gmail.com>
2022-09-20 18:29:33 +0300

    testr

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{    class Program
+    {
+        class Student
+        {
+            public int number = 14;
+            public int GetProperties()
+            {
+                return number;
+            }
+        };
+    static void Main(string[] args)
+    {
+        Console.Write("Please,enter your name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine("Your name is: "+ name);
+        Student student = new Student();
+        int number = student.GetProperties();
+        Console.Write(number); 
+        
+
+    }
+    }
+}
+
